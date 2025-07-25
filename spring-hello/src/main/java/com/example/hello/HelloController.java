b03169239e8073f9d88b6ac1a6c9ccc7d3530124
Level 1 Support <support_l1@vtcholding.com>
2025-07-25 17:11:57 +0800

    update

--- spring-hello/src/main/java/com/example/hello/HelloController.java
+++ spring-hello/src/main/java/com/example/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "<h1>Hello, World!</h1>";
+        return "<h1>Welcome to Spring Boot!</h1>";
     }
 }
