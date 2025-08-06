00f3142de715acb9e6be7c074d6a4eeb18e8be7c
Level 1 Support <support_l1@vtcholding.com>
2025-08-06 14:29:17 +0800

    Correction

--- spring-hello/playwright-tests/playwright.config.js
+++ spring-hello/playwright-tests/playwright.config.js
@@ -1,6 +1,6 @@
 // playwright.config.js
 module.exports = {
   use: {
-    baseURL: 'http://localhost:9090',
-  },
+  baseURL: 'http://localhost:9090',
+}
 };
--- spring-hello/src/main/java/com/example/hello/HelloController.java
+++ spring-hello/src/main/java/com/example/hello/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
-        return "<h1>Spring Boot!</h1>";
+        return "Hello, World!";
     }
 }
