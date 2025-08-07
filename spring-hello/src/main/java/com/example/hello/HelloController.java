54832cd94518b1fb22fd6aa670e6d8926a7c77ed
Level 1 Support <support_l1@vtcholding.com>
2025-08-07 14:49:15 +0800

    Update

--- spring-hello/src/main/java/com/example/hello/HelloController.java
+++ spring-hello/src/main/java/com/example/hello/HelloController.java
@@ -1,7 +1,6 @@
-package com.example.hello;
+package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -10,5 +9,3 @@ public class HelloController {
         return "Hello World";
     }
 }
-
-
