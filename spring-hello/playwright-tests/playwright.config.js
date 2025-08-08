162c57da0f943e9291a2c6f9f68b6a51f6a6cfe9
Level 1 Support <support_l1@vtcholding.com>
2025-08-08 14:38:16 +0800

    Update

--- spring-hello/playwright-tests/playwright.config.js
+++ spring-hello/playwright-tests/playwright.config.js
@@ -1,26 +1,14 @@
+// spring-hello/playwright-tests/playwright.config.js
 const { defineConfig } = require('@playwright/test');
-
-// playwright.config.js
-const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   projects: [
-    {
-      name: 'Chromium',
-      use: { browserName: 'chromium' },
-    },
-    {
-      name: 'Firefox',
-      use: { browserName: 'firefox' },
-    },
-    {
-      name: 'WebKit',
-      use: { browserName: 'webkit' },
-    },
+    { name: 'Chromium', use: { browserName: 'chromium' } },
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+    { name: 'WebKit', use: { browserName: 'webkit' } }
   ],
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:9090',
     headless: true
   },
 });
-
